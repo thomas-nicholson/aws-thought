cd5298295c49abe4ad7f6ceaa0f55dc39570fbd1
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-09-19 18:48:51 +1000

    Changed Bucket region

--- server/create-bucket.js
+++ server/create-bucket.js
@@ -2,7 +2,7 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 // Set the region
-AWS.config.update({region: 'ap-southeast-2'});
+AWS.config.update({region: 'us-east-2'});
 
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
